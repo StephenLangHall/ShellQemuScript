bfbdd316f8d471b5880a807135469f2112bcec7c
Stephen Hall <134156722+StephenLangHall@users.noreply.github.com>
2023-08-14 13:57:04 -0400

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+wd=$(echo $PWD)
+
+mkdir img iso run
+echo "#!/bin/bash
+if [ \"\$1\" == \"new\" ]; then
+
+	read -p \"Disk Name: \" image
+	read -p \"Disk Size (MB): \" disksize
+	read -p \"Iso: \" iso
+	read -p \"Ram (MB): \" ram
+	read -p \"Cores: \" cores
+	
+	sudo qemu-img create $rd/img/\$image.img \${disksize}M
+	sudo qemu-system-x86_64 -smp cores=\$cores -boot d -cdrom $rd/iso/\$iso.iso -m \$ram -hda $rd/img/\$image.img
+
+	sudo echo \"#!/bin/bash
+sudo qemu-system-x86_64 -m \$ram -smp cores=\$cores -drive format=raw,file=$rd/img/\$name.img
+	\" > $rd/run/run\$image.sh
+	sudo chmod +x $rd/run/run\$image.sh
+
+elif [ \"\$1\" == \"run\" ]; then
+
+	read -p \"Boot into: \" image
+	sudo $rd/run/run\$image.sh
+
+elif [ \"\$1\" == \"del\" ]; then
+
+	read -p \"Delete: \" image
+	sudo rm $rd/img/\$image.img
+	sudo rm $rd/run/run\$image.sh
+
+fi
+" > vm
+chmod +x vm
