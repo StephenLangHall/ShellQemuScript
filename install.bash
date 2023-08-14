54afdf5d0a181c17fa666408aeccc6643a2b7fd6
Stephen Hall <134156722+StephenLangHall@users.noreply.github.com>
2023-08-14 15:41:37 -0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -18,17 +18,19 @@ if [ \"\$1\" == \"new\" ]; then
 	sudo qemu-system-\$cpuarch -smp cores=\$cores -boot d -cdrom $wd/iso/\$iso.iso -m \$ram -hda $wd/img/\$image.img
 
 	sudo echo \"#!/bin/bash
-sudo qemu-system-\$cpuarch -m \$ram -smp cores=\$cores -drive format=raw,file=$wd/img/\$name.img
+sudo qemu-system-\$cpuarch -m \$ram -smp cores=\$cores -drive format=raw,file=$wd/img/\$image.img
 	\" > $wd/run/run\$image.sh
 	sudo chmod +x $wd/run/run\$image.sh
 
 elif [ \"\$1\" == \"run\" ]; then
 
+ 	ls $wd/img
 	read -p \"Boot into: \" image
 	sudo $wd/run/run\$image.sh
 
 elif [ \"\$1\" == \"del\" ]; then
 
+ 	ls $wd/img
 	read -p \"Delete: \" image
 	sudo rm $wd/img/\$image.img
 	sudo rm $wd/run/run\$image.sh
